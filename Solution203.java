76f30278ac82b87d68bd6c581af9b2a4fd4f365c
xyu98 <18810509729@163.com>
2021-04-06 10:09:16 +0800

    Create Solution203.java

---
+++ Solution203.java
@@ -0,0 +1,78 @@
+import java.util.LinkedList;
+
+public class Solution203 {
+    public static void main(String[] args) {
+        Solution203_1 s = new Solution203_1();
+        // 题目中只给了ListNode，不能用现成的LinkedList链表的方法
+        // 遍历只能用结点及其next指针
+        // LinkedList<ListNode> l1 = new LinkedList<>();
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(7);
+        ListNode node4 = new ListNode(7);
+        ListNode iter = node1; // 用于遍历
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        /*l1.add(node1);
+        l1.add(node2);
+        l1.add(node3);
+        l1.add(node4);*/
+        ListNode head = new ListNode();
+        head = s.removeElements(node1, 7);
+        // 遍历输出
+        while (head != null) {
+            System.out.println(head);
+            head = head.next;
+        }
+    }
+}
+
+class ListNode {
+    int val;        // 当前节点的内容
+    ListNode next;  // 下一个节点的指针
+    ListNode() {}   // 无参构造方法
+    ListNode(int val) { this.val = val; }  // 构造方法
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }  // 构造方法
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                '}';
+    }
+}
+
+// 第1次提交，执行用时: 1 ms（超过99.84%），内存消耗: 39.2 MB MB（超过85.31%）
+class Solution203_1 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode tempHead = head; // 指向头节点
+        ListNode prev = head;     // 当前操作对象的前一个节点
+        if (tempHead == null) {
+            return null;
+        }
+
+        // 头节点需要删除的情况
+        while (tempHead.val == val) {
+            if (tempHead.next != null) {
+                tempHead = tempHead.next;
+                prev = tempHead;
+            }
+            else {
+                return null;
+            }
+        }
+
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            }
+            else {
+                prev = prev.next;
+            }
+        }
+
+        return tempHead;
+    }
+}
+
