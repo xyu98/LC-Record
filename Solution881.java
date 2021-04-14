5e2bca52d21621900ce6361876b8134760b1ee37
xyu <75960264+xyu98@users.noreply.github.com>
2021-04-14 18:35:30 +0800

    Create Solution881.java

---
+++ Solution881.java
@@ -0,0 +1,16 @@
+public class Solution881 {
+    public static void main(String[] args) {
+        Solution881_1 s = new Solution881_1();
+        int[] people = {3,5,3,4};
+        int limit = 5;
+        System.out.println(s.numRescueBoats(people,limit));
+    }
+}
+
+class Solution881_1 {
+    public int numRescueBoats(int[] people, int limit) {
+        int cnt = 0;
+
+        return cnt;
+    }
+}
