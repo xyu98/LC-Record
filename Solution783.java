03d3876af8c9063585eb9a2120ec0e1a062d6118
xyu98 <18810509729@163.com>
2021-04-15 15:51:25 +0800

    Create Solution783.java

---
+++ Solution783.java
@@ -0,0 +1,28 @@
+public class Solution783 {
+    public static void main(String[] args) {
+        //Solution783_1 s = new Solution783_1();
+        // 要建立一棵二叉搜索树，不写测试程序了
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/*
+class Solution783_1 {
+    public int minDiffInBST(TreeNode root) {
+
+    }
+}
+
+ */
