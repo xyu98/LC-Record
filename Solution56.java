68909de3b2ccc536ee370eb5b4e308a5fbc11f80
xyu98 <18810509729@163.com>
2021-04-24 23:17:31 +0800

    Create Solution56.java

---
+++ Solution56.java
@@ -0,0 +1,14 @@
+public class Solution56 {
+    public static void main(String[] args) {
+        Solution56_1 s = new Solution56_1();
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        System.out.println(s.merge(intervals));
+    }
+}
+
+class Solution56_1 {
+    public int[][] merge(int[][] intervals) {
+        int[][] matrix = new int[1][1];
+        return matrix;
+    }
+}
