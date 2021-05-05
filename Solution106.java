55a9439893e073011ec5ee747eae8aaca4505ee1
xyu98 <18810509729@163.com>
2021-05-05 18:56:54 +0800

    Create Solution106.java

---
+++ Solution106.java
@@ -0,0 +1,17 @@
+/*
+public class Solution106 {
+    public static void main(String[] args) {
+        Solution106_1 s = new Solution106_1();
+        int[] inorder = {9,3,15,20,7};
+        int[] postorder = {9,15,7,20,3};
+        System.out.println(s.buildTree(inorder, postorder));
+    }
+}
+
+class Solution106_1 {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+    }
+}
+
+ */
