c07729c2d0ff960fa5d44fe5c804b9ae5ed8c1c6
xyu98 <18810509729@163.com>
2021-05-07 09:14:02 +0800

    Create Solution236.java

---
+++ Solution236.java
@@ -0,0 +1,21 @@
+public class Solution236 {
+    public static void main(String[] args) {
+        Solution236_1 s = new Solution236_1();
+        TreeNode t1 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(9);
+        TreeNode t3 = new TreeNode(20);
+        TreeNode t4 = new TreeNode(15);
+        TreeNode t5 = new TreeNode(7);
+        t1.left = t2;
+        t1.right = t3;
+        t3.left = t4;
+        t3.right = t5;
+        System.out.println(s.lowestCommonAncestor(t1, t4, t5));
+    }
+}
+
+class Solution236_1 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+    }
+}
