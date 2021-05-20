1c460b6423bf850c4eae1438597173531c6b3c5e
xyu98 <18810509729@163.com>
2021-05-20 15:37:37 +0800

    Create Solution105.java

---
+++ Solution105.java
@@ -0,0 +1,28 @@
+// build a tree through pre-order and in-order arrays.
+public class Solution105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder, 0, preorder.length - 1,
+                inorder, 0, inorder.length - 1);
+    }
+
+    public TreeNode build(int[] preorder, int preStart, int preEnd,
+                          int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd) return null;
+
+        int rootValue = preorder[preStart];
+        //寻找root.value在inorder中的位置，利用inorder划分左右子树的范围
+        int rootPos;
+        for (rootPos = inStart; rootPos < inorder.length; rootPos++) {
+            if (inorder[rootPos] == rootValue) break;
+        }
+        int leftSize = rootPos - inStart;
+        TreeNode root = new TreeNode(rootValue);
+
+        root.left = build(preorder, preStart + 1, preStart + leftSize,
+                inorder, inStart, rootPos - 1); //除去根节点，故preorder的指针从preStart + 1开始
+        root.right = build(preorder, preStart + leftSize + 1, preEnd,
+                inorder, rootPos + 1, inEnd);
+
+        return root;
+    }
+}
