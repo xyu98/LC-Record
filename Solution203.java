e8ff08360b9aa2a61866d829571db6a5d6587f60
xyu98 <18810509729@163.com>
2021-05-20 15:37:48 +0800

    Update Solution203.java

--- Solution203.java
+++ Solution203.java
@@ -29,21 +29,6 @@ public class Solution203 {
     }
 }
 
-class ListNode {
-    int val;        // 当前节点的内容
-    ListNode next;  // 下一个节点的指针
-    ListNode() {}   // 无参构造方法
-    ListNode(int val) { this.val = val; }  // 构造方法
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }  // 构造方法
-
-    @Override
-    public String toString() {
-        return "ListNode{" +
-                "val=" + val +
-                '}';
-    }
-}
-
 // 第1次提交，执行用时: 1 ms（超过99.84%），内存消耗: 39.2 MB MB（超过85.31%）
 class Solution203_1 {
     public ListNode removeElements(ListNode head, int val) {
