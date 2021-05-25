2b92388766830cb1ea0fb50fb8b04ed917b23089
xyu98 <18810509729@163.com>
2021-05-25 23:02:10 +0800

    Update TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -1,9 +1,13 @@
 public class TreeNode {
     int val;
+    String str;
     TreeNode left;
     TreeNode right;
     TreeNode() {}
     TreeNode(int val) { this.val = val; }
+
+    //TreeNode(String str) { this.str = str; }
+
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
