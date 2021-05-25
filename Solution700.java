71c79b3450f0024c9ccfb84d5ea48591069892c1
xyu98 <18810509729@163.com>
2021-05-25 23:03:07 +0800

    Create Solution700.java

---
+++ Solution700.java
@@ -0,0 +1,11 @@
+public class Solution700 {
+}
+
+class Solution700_1 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        if (root.val < val) return searchBST(root.right, val);
+        else return searchBST(root.left, val);
+    }
+}
