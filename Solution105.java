16e6063b8c44fcf5489e35baaecca4a486980471
xyu98 <18810509729@163.com>
2021-05-25 23:02:42 +0800

    Update Solution105.java

--- Solution105.java
+++ Solution105.java
@@ -9,7 +9,7 @@ public class Solution105 {
                           int[] inorder, int inStart, int inEnd) {
         if (preStart > preEnd) return null;
 
-        int rootValue = preorder[preStart];
+        int rootValue = preorder[preStart];  //注意这里下标不能是0
         //寻找root.value在inorder中的位置，利用inorder划分左右子树的范围
         int rootPos;
         for (rootPos = inStart; rootPos < inorder.length; rootPos++) {
@@ -18,11 +18,10 @@ public class Solution105 {
         int leftSize = rootPos - inStart;
         TreeNode root = new TreeNode(rootValue);
 
-        root.left = build(preorder, preStart + 1, preStart + leftSize,
+        root.left = build(preorder, preStart + 1, preStart + leftSize, //注意+1
                 inorder, inStart, rootPos - 1); //除去根节点，故preorder的指针从preStart + 1开始
         root.right = build(preorder, preStart + leftSize + 1, preEnd,
                 inorder, rootPos + 1, inEnd);
-
         return root;
     }
 }
