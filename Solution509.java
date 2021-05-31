2e0e6a36c26f1f1861f3c93fa6a26d1a5d9d3a7f
xyu98 <18810509729@163.com>
2021-05-31 21:56:39 +0800

    Update Solution509.java

--- Solution509.java
+++ Solution509.java
@@ -12,3 +12,18 @@ class Solution509_1 {
         return fib(n - 1) + fib(n - 2);
     }
 }
+
+class Solution509_2 {
+    public int fib(int n) {
+        if (n < 1) return 0;
+        if (n == 1 || n == 2) return 1;
+        int pre = 1, cur = 1;
+        int sum = 0;
+        for (int i = 3; i <= n; i++) {
+            sum = pre + cur;
+            pre = cur;
+            cur = sum;
+        }
+        return sum;
+    }
+}
