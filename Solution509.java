834a3ea014275899f3eff3a22a39caa7171d61c5
xyu98 <18810509729@163.com>
2021-06-19 22:40:28 +0800

    Update Solution509.java

--- Solution509.java
+++ Solution509.java
@@ -14,6 +14,20 @@ class Solution509_1 {
 }
 
 class Solution509_2 {
+    public int fib(int n){
+        if (n == 1) return 1;
+        if (n == 0) return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
+
+class Solution509_3 {
     public int fib(int n) {
         if (n < 1) return 0;
         if (n == 1 || n == 2) return 1;
