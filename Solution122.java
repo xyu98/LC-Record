94050d76ded3658447167de16c8ace2aba51de4b
xyu98 <18810509729@163.com>
2021-06-26 10:44:10 +0800

    Update Solution122.java

--- Solution122.java
+++ Solution122.java
@@ -1,8 +1,8 @@
 public class Solution122 {
     public static void main(String[] args) {
-        Solution122_1 s = new Solution122_1();
+        Solution122_2 s = new Solution122_2();
         int[] prices = {7,1,5,3,6,4};
-        s.maxProfit(prices);
+        System.out.println(s.maxProfit(prices));
     }
 }
 
@@ -19,5 +19,20 @@ class Solution122_1 {
             j++;
         }
         return res;
+    }
+}
+
+// 解2：动规
+class Solution122_2 {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 1) return 0;
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]); // 多次买卖和单次的不同之处，要加上之前的利润
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return dp[prices.length - 1][1];
     }
 }
