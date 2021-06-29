3e9f4e79986c654832fbc946025597bb473d847b
xyu98 <18810509729@163.com>
2021-06-29 11:02:16 +0800

    Update Solution53.java

--- Solution53.java
+++ Solution53.java
@@ -1,4 +1,9 @@
 public class Solution53 {
+    public static void main(String[] args) {
+        Solution53_2 s = new Solution53_2();
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(s.maxSubArray(nums));
+    }
 }
 
 class Solution53_1 {
@@ -12,5 +17,18 @@ class Solution53_1 {
             if (sum < 0) sum = 0;
         }
         return res;
+    }
+}
+
+class Solution53_2 {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            result = Math.max(dp[i], result);
+        }
+        return result;
     }
 }
