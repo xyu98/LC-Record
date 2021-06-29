963dfd3d709627d1842e675b66d986584d28d89d
xyu98 <18810509729@163.com>
2021-06-29 11:01:58 +0800

    Create Solution1143.java

---
+++ Solution1143.java
@@ -0,0 +1,18 @@
+public class Solution1143 {
+}
+
+class Solution1143_1 {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        int i, j;
+        for (i = 1; i <= text1.length(); i++) {
+            for (j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+}
