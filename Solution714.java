3048743f50567121947b1f2b532e25eea566c40c
xyu98 <18810509729@163.com>
2021-06-29 11:02:22 +0800

    Update Solution714.java

--- Solution714.java
+++ Solution714.java
@@ -1,12 +1,16 @@
 public class Solution714 {
 }
 
-/*
 class Solution714_1 {
     public int maxProfit(int[] prices, int fee) {
-        int low = 0, high = 0;
-        for (int i = 0; i < prices.length; i++) {
-
+        if (prices.length == 1) return 0;
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0] - fee;
+        dp[0][1] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
         }
+        return dp[prices.length - 1][1];
     }
-}*/
+}
