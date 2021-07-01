4fbde29b452abaf9120605f88aaaed1d80df32d3
xyu98 <18810509729@163.com>
2021-07-01 08:23:53 +0800

    Update Solution704.java

--- Solution704.java
+++ Solution704.java
@@ -30,3 +30,19 @@ class Solution704_2 {
         return -1;
     }
 }
+
+
+// 2021.6.30 review
+class Solution704_1_1 {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
