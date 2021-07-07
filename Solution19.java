0383e2c98ac742567dcfe4394bafb9a0048e3e44
xyu98 <18810509729@163.com>
2021-07-07 19:30:45 +0800

    Create Solution19.java

---
+++ Solution19.java
@@ -0,0 +1,19 @@
+public class Solution19 {
+}
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) return null;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode slow = dummyHead, fast = dummyHead, pre = slow;
+        for (int i = 0; i < n; i++) fast = fast.next;
+        while (fast != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next;
+        } // 此时slow就是需要删掉的节点
+        pre.next = slow.next;
+        return dummyHead.next;
+    }
+}
