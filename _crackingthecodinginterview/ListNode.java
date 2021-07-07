89db0f493903d8f1816958c294911e279a775b4e
xyu98 <18810509729@163.com>
2021-07-07 19:30:32 +0800

    Create ListNode.java

---
+++ _crackingthecodinginterview/ListNode.java
@@ -0,0 +1,16 @@
+package _crackingthecodinginterview;
+
+public class ListNode {
+    int val;        // 当前节点的内容
+    ListNode next;  // 下一个节点的指针
+    ListNode() {}   // 无参构造方法
+    ListNode(int val) { this.val = val; }  // 构造方法
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }  // 构造方法
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                '}';
+    }
+}
