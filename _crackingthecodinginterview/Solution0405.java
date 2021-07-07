36a985a2950aabcde60780cf20598edafe626a12
xyu98 <18810509729@163.com>
2021-07-07 19:30:37 +0800

    Create Solution0405.java

---
+++ _crackingthecodinginterview/Solution0405.java
@@ -0,0 +1,16 @@
+public class Solution0405 {
+}
+
+// 同leetcode98题一样
+// 注意是后序遍历！！
+class Solution0405_1 {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val <= min || root.val >= max) return false;
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+}
