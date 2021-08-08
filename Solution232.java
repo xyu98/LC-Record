9c8ef225db9a641f8665f2195323d5029ac959a3
xyu98 <18810509729@163.com>
2021-08-08 08:45:43 +0800

    Create Solution232.java

---
+++ Solution232.java
@@ -0,0 +1,60 @@
+import java.util.Stack;
+
+public class Solution232 {
+}
+
+class MyQueue {
+    private Stack<Integer> stack1; // 输入栈
+    private Stack<Integer> stack2; // 输出栈
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        // 注意，satck2中的元素不必返回stack中，两个栈共同组成完整的队列
+        /*int pk = stack2.peek();
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        return pk;*/
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
